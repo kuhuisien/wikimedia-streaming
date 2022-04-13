38900e39aef99b5f3bde5404106011044d76068b
kuhuisien <>
2022-04-13 09:22:43 +0800

    refine kafka consumer to do batch processing

--- kafka-consumer-opensearch/src/main/java/kafka/wikimedia/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/kafka/wikimedia/OpenSearchConsumer.java
@@ -1,5 +1,6 @@
 package kafka.wikimedia;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -7,18 +8,27 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.bulk.BulkRequest;
+import org.opensearch.action.bulk.BulkResponse;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
 import java.util.Properties;
 
 public class OpenSearchConsumer {
@@ -68,8 +78,20 @@ public class OpenSearchConsumer {
         // onwards it starts reading from offset where it is committed the readConsumerDemo
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
+        // disable auto commit to commit manually
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+
         // create consumer
         return new KafkaConsumer<>(properties);
+    }
+
+    private static String extractId(String json) {
+        return JsonParser.parseString(json)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
     }
 
     public static void main(String[] args) throws IOException {
@@ -92,6 +114,59 @@ public class OpenSearchConsumer {
                 log.info("wikimedia index has been created");
             } else {
                 log.info("wikimedia index already exists");
+            }
+
+            // subscribe consumer
+            kafkaConsumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(3000));
+
+                int recordCount = records.count();
+                log.info("Received " + recordCount + " record(s)");
+
+                // batch processing for efficiency
+                BulkRequest bulkRequest = new BulkRequest();
+
+                for (ConsumerRecord<String, String> record: records) {
+
+                    // send record into OpenSearch
+                    try {
+                        // make consumer idempotent
+                        // if same id, update the same record, hence no duplicated record existing
+                        String id = extractId(record.value());
+
+                        IndexRequest indexRequest = new IndexRequest("wikimedia")
+                                .source(record.value(), XContentType.JSON)
+                                .id(id);
+
+                        bulkRequest.add(indexRequest);
+
+                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+
+                        log.info(response.getId());
+                    } catch (Exception exception) {
+                        
+                    }
+                }
+
+                // batch processing
+                if (bulkRequest.numberOfActions() > 0) {
+                    BulkResponse bulkResponse = openSearchClient.bulk(
+                            bulkRequest, RequestOptions.DEFAULT);
+                    log.info("Inserted " + bulkResponse.getItems().length + " record(s).");
+
+                    // add delay to increase batch size
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException exception) {
+                        exception.printStackTrace();
+                    }
+
+                    kafkaConsumer.commitSync();
+                    log.info("offsets have been committed");
+
+                }
             }
 
         }
